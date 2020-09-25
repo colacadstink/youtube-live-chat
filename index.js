4eb3f9b634a6ca60cfd7221098b483ecfed942da
Ben Harris <benharris5@gmail.com>
2020-09-24 22:55:43 -0400

    Switched from request to fetch, and added some improvements to allow for a client selecting *which* live to use if multiple exist, optionally disabling notifications of past messages when connecting to a chat, and keeping track of if we're actively watching a chat or not. Also upgraded the eslint parser to babel so it could understand static class members, and my IDE settings to the .gitignore

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const request = require('request')
 const {EventEmitter} = require('events')
+const fetch = require('node-fetch')
 
 /**
  * The main hub for acquire live chat with the YouTube Date API.
  * @extends {EventEmitter}
  */
 class YouTube extends EventEmitter {
+  static CANNOT_FIND_LIVE = 'Cannot find live.'
+  active = false
+  
   /**
-   * @param {string} ChannelID ID of the channel to acquire with
-   * @param {string} APIKey You'r API key
+   * @param {string} channelId ID of the channel to acquire with
+   * @param {string} apiKey Your API key
    */
   constructor(channelId, apiKey) {
     super()
@@ -16,30 +19,35 @@ class YouTube extends EventEmitter {
     this.key = apiKey
     this.getLive()
   }
-
+  
   getLive() {
     const url = 'https://www.googleapis.com/youtube/v3/search'+
-      '?eventType=live'+
-      '&part=id'+
-      `&channelId=${this.id}`+
-      '&type=video'+
-      `&key=${this.key}`
+        '?eventType=live'+
+        '&part=id'+
+        `&channelId=${this.id}`+
+        '&type=video'+
+        `&key=${this.key}`
     this.request(url, data => {
-      if (!data.items[0])
-        this.emit('error', 'Can not find live.')
-      else {
+      if (!data.items[0]) {
+        this.emit('error', YouTube.CANNOT_FIND_LIVE)
+      } else if (data.items.length > 1) {
+        this.emit('multilive', data.items, (item) => {
+          this.liveId = item.id.videoId
+          this.getChatId()
+        })
+      } else {
         this.liveId = data.items[0].id.videoId
         this.getChatId()
       }
     })
   }
-
+  
   getChatId() {
     if (!this.liveId) return this.emit('error', 'Live id is invalid.')
     const url = 'https://www.googleapis.com/youtube/v3/videos'+
-      '?part=liveStreamingDetails'+
-      `&id=${this.liveId}`+
-      `&key=${this.key}`
+        '?part=liveStreamingDetails'+
+        `&id=${this.liveId}`+
+        `&key=${this.key}`
     this.request(url, data => {
       if (!data.items.length)
         this.emit('error', 'Can not find chat.')
@@ -49,7 +57,7 @@ class YouTube extends EventEmitter {
       }
     })
   }
-
+  
   /**
    * Gets live chat messages.
    * See {@link https://developers.google.com/youtube/v3/live/docs/liveChatMessages/list#response|docs}
@@ -58,37 +66,42 @@ class YouTube extends EventEmitter {
   getChat() {
     if (!this.chatId) return this.emit('error', 'Chat id is invalid.')
     const url = 'https://www.googleapis.com/youtube/v3/liveChat/messages'+
-      `?liveChatId=${this.chatId}`+
-      '&part=id,snippet,authorDetails'+
-      '&maxResults=2000'+
-      `&key=${this.key}`
+        `?liveChatId=${this.chatId}`+
+        '&part=id,snippet,authorDetails'+
+        '&maxResults=2000'+
+        `&key=${this.key}`
     this.request(url, data => {
       this.emit('json', data)
     })
   }
 
   request(url, callback) {
-    request({
-      url: url,
-      method: 'GET',
-      json: true,
-    }, (error, response, data) => {
-      if (error)
-        this.emit('error', error)
-      else if (response.statusCode !== 200)
-        this.emit('error', data)
-      else
-        callback(data)
-    })
+    fetch(url)
+      .then(res => {
+        if (res.ok) {
+          return res.json()
+        } else {
+          this.emit('error', res)
+        }
+      })
+      .then(json => {
+        callback(json)
+      })
+      .catch(err => {
+        this.emit('error', err)
+      })
   }
-
+  
   /**
    * Gets live chat messages at regular intervals.
    * @param {number} delay Interval to get live chat messages
+   * @param {boolean} ignoreOld Should we ignore all messages sent before we started?
    * @fires YouTube#message
    */
-  listen(delay) {
+  listen(delay, ignoreOld) {
     let lastRead = 0, time = 0
+    this.active = true
+    this.getChat()
     this.interval = setInterval(() => this.getChat(), delay)
     this.on('json', data => {
       for (const item of data.items) {
@@ -96,21 +109,26 @@ class YouTube extends EventEmitter {
         if (lastRead < time) {
           lastRead = time
           /**
-          * Emitted whenever a new message is recepted.
-          * See {@link https://developers.google.com/youtube/v3/live/docs/liveChatMessages#resource|docs}
-          * @event YouTube#message
-          * @type {object}
-          */
-          this.emit('message', item)
+           * Emitted whenever a new message is received.
+           * See {@link https://developers.google.com/youtube/v3/live/docs/liveChatMessages#resource|docs}
+           * @event YouTube#message
+           * @type {object}
+           */
+          if (!ignoreOld) {
+            // Only emit if we're not ignoring things
+            this.emit('message', item)
+          }
         }
       }
+      ignoreOld = false // Always listen after the first run no matter what
     })
   }
-
+  
   /**
    * Stops getting live chat messages at regular intervals.
    */
   stop() {
+    this.active = false
     clearInterval(this.interval)
   }
 }
